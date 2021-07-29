90c52e33d0e35f712e002b85ba91c33d260d10f5
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-07-29 10:34:47 +0300

    Update hr_attendance.py

--- inventory_excel/models/hr_attendance.py
+++ inventory_excel/models/hr_attendance.py
@@ -62,7 +62,7 @@ class ResPartner(models.Model):
     def import_data(self, part_master_id=False):
         if part_master_id:
             part_master = self.env[
-                'import.attendances.master'].browse(part_master_id)
+                'import.inventory.master'].browse(part_master_id)
             total_success_import_record = 0
             total_failed_record = 0
             list_of_failed_record = ''
@@ -93,132 +93,135 @@ class ResPartner(models.Model):
                     data_list = []
                     header_list = []
                     headers_dict = {}
-                    for sheet in wb.sheets():
-                        _logger.info(sheet.nrows)
-                    #     # Sales data xlsx
-                        first_row = 0
-                        emp_name_row = 0
-                        date_row = 0
-                        check_in_row = 0
-                        check_out_row = 0
-                        for rownum in range(sheet.nrows):
-                            # _logger.info(rownum)
-                            # _logger.info(sheet.row_values(rownum))
-                            item = sheet.row_values(rownum)
-                            if "اسم الموظف" in item and "التاريخ" in item and "وقت دخول" in item and "وقت الخروج" in item:
-                                first_row = rownum
-                                for idx1,item1 in enumerate(item):
-                                    if item1 == "اسم الموظف":
-                                        emp_name_row = idx1
-                                    if item1 == "التاريخ":
-                                        date_row = idx1
-                                    if item1 == "وقت دخول":
-                                        check_in_row = idx1
-                                    if item1 == "وقت الخروج":
-                                        check_out_row = idx1
-                                break    
+                    sheet_names = wb.sheet_names()
+                    _logger.info("list_of_failed_record")
+                    _logger.info(sheet_names)
+                    # for sheet in wb.sheets():
+                    #     _logger.info(sheet.nrows)
+                    # #     # Sales data xlsx
+                    #     first_row = 0
+                    #     emp_name_row = 0
+                    #     date_row = 0
+                    #     check_in_row = 0
+                    #     check_out_row = 0
+                    #     for rownum in range(sheet.nrows):
+                    #         # _logger.info(rownum)
+                    #         # _logger.info(sheet.row_values(rownum))
+                    #         item = sheet.row_values(rownum)
+                    #         if "اسم الموظف" in item and "التاريخ" in item and "وقت دخول" in item and "وقت الخروج" in item:
+                    #             first_row = rownum
+                    #             for idx1,item1 in enumerate(item):
+                    #                 if item1 == "اسم الموظف":
+                    #                     emp_name_row = idx1
+                    #                 if item1 == "التاريخ":
+                    #                     date_row = idx1
+                    #                 if item1 == "وقت دخول":
+                    #                     check_in_row = idx1
+                    #                 if item1 == "وقت الخروج":
+                    #                     check_out_row = idx1
+                    #             break    
 
-                        for rownum1 in range(sheet.nrows): 
-                            item_y = sheet.row_values(rownum1)           
-                            if rownum1 > first_row:
-                                emp_name =  item_y[emp_name_row]
-                                date = item_y[date_row] 
-                                check_in = item_y[check_in_row]     
-                                check_out = item_y[check_out_row] 
-                                full_date_check_in = False
-                                full_date_check_out = False
-                                if emp_name != "" and (check_in != "" or check_out != ""):  
-                                    if check_in:
-                                        split_check_in = check_in.split(" ")
-                                        if len(split_check_in) > 1:
-                                            try:
-                                                date = date.replace("/","-",3)
-                                                new_time = self.pool.get("hr.attendance").convert24(self,split_check_in)
-                                                new_time = new_time.replace("\u200f","")
-                                                full_date_time = date + " " + new_time + ":00"
-                                                full_date_check_in = datetime.strptime(full_date_time, '%Y-%m-%d %H:%M:%S')
-                                            except Exception as e:
-                                                _logger.info("----error in------")   
-                                                _logger.info(e) 
-                                    if check_out:
-                                        split_check_out = check_out.split(" ")
-                                        if len(split_check_out) > 1:  
-                                            try:
-                                                date = date.replace("/","-",3)
-                                                new_time = self.pool.get("hr.attendance").convert24(self,split_check_out)
-                                                full_date_time = date + " " + new_time + ":00"
-                                                full_date_check_out = datetime.strptime(full_date_time, '%Y-%m-%d %H:%M:%S')
-                                            except Exception as e:
-                                                _logger.info("----error out------")    
-                                                _logger.info(e)   
-                                # if  full_date_check_in != False or full_date_check_out != False:
-                                if  full_date_check_in != False :
-                                    try:
-                                        emp_name_info = self.env['hr.employee'].sudo().search([('name','=',emp_name)])
-                                        attendance_vals = {
-                                            'employee_id': emp_name_info.id,
-                                            'check_in': full_date_check_in,
-                                            'check_out': full_date_check_out
-                                        }
-                                        self.env['hr.attendance'].sudo().create(attendance_vals)
-                                        _logger.info(full_date_check_in)
-                                        _logger.info(full_date_check_out)
-                                        _logger.info(emp_name)
-                                        _logger.info(emp_name_info)  
-                                        total_success_import_record += 1  
-                                    except Exception as e:
-                                        total_failed_record += 1
-                                        list_of_failed_record += "| Error at Line :" + str(rownum1 + 1) + " |"
-                                        _logger.error("Error at %s" % str(rownum1))    
-                                else:
-                                    if emp_name != "":
-                                        total_failed_record += 1
-                                        list_of_failed_record += "| Error at Line :" + str(rownum1 + 1) + " |"
-                                        _logger.error("Error at %s" % str(rownum1))                         
+                    #     for rownum1 in range(sheet.nrows): 
+                    #         item_y = sheet.row_values(rownum1)           
+                    #         if rownum1 > first_row:
+                    #             emp_name =  item_y[emp_name_row]
+                    #             date = item_y[date_row] 
+                    #             check_in = item_y[check_in_row]     
+                    #             check_out = item_y[check_out_row] 
+                    #             full_date_check_in = False
+                    #             full_date_check_out = False
+                    #             if emp_name != "" and (check_in != "" or check_out != ""):  
+                    #                 if check_in:
+                    #                     split_check_in = check_in.split(" ")
+                    #                     if len(split_check_in) > 1:
+                    #                         try:
+                    #                             date = date.replace("/","-",3)
+                    #                             new_time = self.pool.get("hr.attendance").convert24(self,split_check_in)
+                    #                             new_time = new_time.replace("\u200f","")
+                    #                             full_date_time = date + " " + new_time + ":00"
+                    #                             full_date_check_in = datetime.strptime(full_date_time, '%Y-%m-%d %H:%M:%S')
+                    #                         except Exception as e:
+                    #                             _logger.info("----error in------")   
+                    #                             _logger.info(e) 
+                    #                 if check_out:
+                    #                     split_check_out = check_out.split(" ")
+                    #                     if len(split_check_out) > 1:  
+                    #                         try:
+                    #                             date = date.replace("/","-",3)
+                    #                             new_time = self.pool.get("hr.attendance").convert24(self,split_check_out)
+                    #                             full_date_time = date + " " + new_time + ":00"
+                    #                             full_date_check_out = datetime.strptime(full_date_time, '%Y-%m-%d %H:%M:%S')
+                    #                         except Exception as e:
+                    #                             _logger.info("----error out------")    
+                    #                             _logger.info(e)   
+                    #             # if  full_date_check_in != False or full_date_check_out != False:
+                    #             if  full_date_check_in != False :
+                    #                 try:
+                    #                     emp_name_info = self.env['hr.employee'].sudo().search([('name','=',emp_name)])
+                    #                     attendance_vals = {
+                    #                         'employee_id': emp_name_info.id,
+                    #                         'check_in': full_date_check_in,
+                    #                         'check_out': full_date_check_out
+                    #                     }
+                    #                     self.env['hr.attendance'].sudo().create(attendance_vals)
+                    #                     _logger.info(full_date_check_in)
+                    #                     _logger.info(full_date_check_out)
+                    #                     _logger.info(emp_name)
+                    #                     _logger.info(emp_name_info)  
+                    #                     total_success_import_record += 1  
+                    #                 except Exception as e:
+                    #                     total_failed_record += 1
+                    #                     list_of_failed_record += "| Error at Line :" + str(rownum1 + 1) + " |"
+                    #                     _logger.error("Error at %s" % str(rownum1))    
+                    #             else:
+                    #                 if emp_name != "":
+                    #                     total_failed_record += 1
+                    #                     list_of_failed_record += "| Error at Line :" + str(rownum1 + 1) + " |"
+                    #                     _logger.error("Error at %s" % str(rownum1))                         
             except Exception as e:
                 list_of_failed_record += str(e)
             _logger.info("list_of_failed_record")
             _logger.info(list_of_failed_record)
 
 
-            try:
-                file_data = base64.b64encode(
-                    list_of_failed_record.encode('utf-8'))
-                part_master.status = 'imported'
-                datetime_object = datetime.strptime(
-                    str(part_master.create_date), '%Y-%m-%d %H:%M:%S.%f')
-                start_date = datetime.strftime(
-                    datetime_object, DEFAULT_SERVER_DATETIME_FORMAT)
-                self._cr.commit()
-                now_time = datetime.now()
-                user_tz = self.env.user.tz or str(pytz.utc)
-                local = pytz.timezone(user_tz)
-                start_date_in_user_tz = datetime.strftime(pytz.utc.localize(
-                    datetime.strptime(str(start_date), DEFAULT_SERVER_DATETIME_FORMAT)).astimezone(
-                    local), DEFAULT_SERVER_DATETIME_FORMAT)
-                end_date_in_user_tz = datetime.strftime(pytz.utc.localize(
-                    now_time).astimezone(local),
-                    DEFAULT_SERVER_DATETIME_FORMAT)
-                self.env['import.attendances.history'].create({
-                    'total_success_count': total_success_import_record,
-                    'total_failed_count': total_failed_record,
-                    'file': file_data,
-                    'file_name': 'report_importazione.txt',
-                    'type': part_master.type,
-                    'import_file_name': part_master.filename,
-                    'start_date': start_date_in_user_tz,
-                    'end_date': end_date_in_user_tz,
-                    'operation': part_master.operation,
-                })
-                if part_master.user_id:
-                    message = "Import process is completed. Check in Imported attendances History if all the attendances have" \
-                              " been imported correctly. </br></br> Imported File: %s </br>" \
-                              "Imported by: %s" % (
-                                  part_master.filename, part_master.user_id.name)
-                    part_master.user_id.notify_partner_info(
-                        message, part_master.user_id, sticky=True)
-                self._cr.commit()
-            except Exception as e:
-                part_master.status = 'failed'
-                _logger.error(e)
-                self._cr.commit()
+            # try:
+            #     file_data = base64.b64encode(
+            #         list_of_failed_record.encode('utf-8'))
+            #     part_master.status = 'imported'
+            #     datetime_object = datetime.strptime(
+            #         str(part_master.create_date), '%Y-%m-%d %H:%M:%S.%f')
+            #     start_date = datetime.strftime(
+            #         datetime_object, DEFAULT_SERVER_DATETIME_FORMAT)
+            #     self._cr.commit()
+            #     now_time = datetime.now()
+            #     user_tz = self.env.user.tz or str(pytz.utc)
+            #     local = pytz.timezone(user_tz)
+            #     start_date_in_user_tz = datetime.strftime(pytz.utc.localize(
+            #         datetime.strptime(str(start_date), DEFAULT_SERVER_DATETIME_FORMAT)).astimezone(
+            #         local), DEFAULT_SERVER_DATETIME_FORMAT)
+            #     end_date_in_user_tz = datetime.strftime(pytz.utc.localize(
+            #         now_time).astimezone(local),
+            #         DEFAULT_SERVER_DATETIME_FORMAT)
+            #     self.env['import.attendances.history'].create({
+            #         'total_success_count': total_success_import_record,
+            #         'total_failed_count': total_failed_record,
+            #         'file': file_data,
+            #         'file_name': 'report_importazione.txt',
+            #         'type': part_master.type,
+            #         'import_file_name': part_master.filename,
+            #         'start_date': start_date_in_user_tz,
+            #         'end_date': end_date_in_user_tz,
+            #         'operation': part_master.operation,
+            #     })
+            #     if part_master.user_id:
+            #         message = "Import process is completed. Check in Imported attendances History if all the attendances have" \
+            #                   " been imported correctly. </br></br> Imported File: %s </br>" \
+            #                   "Imported by: %s" % (
+            #                       part_master.filename, part_master.user_id.name)
+            #         part_master.user_id.notify_partner_info(
+            #             message, part_master.user_id, sticky=True)
+            #     self._cr.commit()
+            # except Exception as e:
+            #     part_master.status = 'failed'
+            #     _logger.error(e)
+            #     self._cr.commit()
