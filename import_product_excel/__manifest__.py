11889370e7a0131c57c2faa251d8be8e07232837
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-07-29 14:58:08 +0300

    ED

--- import_product_excel/__manifest__.py
+++ import_product_excel/__manifest__.py
@@ -1,7 +1,7 @@
 {
-    'name': 'Product Data Import - XLS',
+    'name': 'Product Data Import - XLSX',
     'version': '13.0.0.1.0',
-    'depends': ["stock","web_notify"],
+    'depends': ["stock","web_notify","om_account_asset"],
     'data': [
         'wizard/import_product_data_view.xml',
         'views/import_product_history_view.xml',
