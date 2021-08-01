1e39505d8bf556ecd0b05474418ed461a837727b
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-01 11:04:11 +0300

    Update __manifest__.py

--- custom_custody/__manifest__.py
+++ custom_custody/__manifest__.py
@@ -3,7 +3,6 @@
     'version': '13.0.1.0.0',
     'depends': ['hr', 'mail', 'om_account_asset', 'product'],
     'data': [
-        'security/ir.model.access.csv',
         # 'security/custody_security.xml',
         # 'views/wizard_reason_view.xml',
         # 'views/custody_view.xml',
@@ -11,6 +10,7 @@
         # 'views/hr_employee_view.xml',
         # 'views/notification_mail.xml',
         # 'reports/custody_report.xml'
+        'security/ir.model.access.csv',
     ],
     # 'images': ['static/description/banner.png'],
     'installable': True,
