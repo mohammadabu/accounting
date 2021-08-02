ded3f57d1bb3195a75aee819a02d1517ea3cd63d
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-02 11:50:34 +0300

    ed

--- custom_hr_custody/models/custody_employee.py
+++ custom_hr_custody/models/custody_employee.py
@@ -5,4 +5,16 @@ from odoo import models, fields, api, _
 class HrEmployeeCustody(models.Model):
     _inherit = 'hr.employee'
 
-    employee_custody = fields.One2many('hr.custody', 'employee')
+
+    @api.model
+    def getAllEmployeeCustody(self):
+        custody = self.env['hr.custody.lines'].sudo().search([])
+        self.employee_custody = custody
+        # if self.parent_opportunity.id != False:
+        #     custody = self.env['hr.custody.lines'].sudo().search(['&','|',('default_access_emails','like','#'+str(self.env.uid)+'#'),'|',('stage_access_emails','like','#'+str(self.env.uid)+'#'),'|',('assigned_resources_access_emails','like','#'+str(self.env.uid)+'#'),('owner_ownerManager_emails','like','#'+str(self.env.uid)+'#'),('parent_opportunity','=',self.parent_opportunity.id),('id','!=',self.id)])
+        #     self.employee_custody = custody
+        # else:
+        #      self.employee_custody = []   
+    # related_project = fields.Many2many('project.project','related_project','name_seq',compute='getAllRelatedProject')
+
+    employee_custody = fields.One2many('hr.custody.lines', 'custody_id',compute='getAllEmployeeCustody')
