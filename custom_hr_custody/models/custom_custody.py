98855b4ca3abdb5d7cee7d5e0620553c5ba338e7
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-02 14:49:53 +0300

    ed

--- custom_hr_custody/models/custom_custody.py
+++ custom_hr_custody/models/custom_custody.py
@@ -38,8 +38,7 @@ class HrCustomCustodyLines(models.Model):
     _name = 'hr.custody.lines' 
     _sql_constraints = [('custody_item', 'unique (custody_id,custody_item)',     
                  'Duplicate Items in Custody line not allowed !')]
-       
-    custody_item = fields.Many2one('hr.custody.items',string="Items") 
+
     custody_qty = fields.Integer(string="Quantity")
     custody_id = fields.Many2one('hr.custody',string="Custody Id") 
     
--- custom_hr_custody/models/custom_items.py
+++ custom_hr_custody/models/custom_items.py
@@ -12,9 +12,18 @@ class HrCustomCustodyItems(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
 
+    @api.model
+    def getItemQuantity(self):
+        product_id = self.products
+        stock_move = self.env['stock.move.line'].sudo().search([('state','=','done'),('product_id','=',product_id)])
+        qty = 0
+        for line in stock_move:
+            qty = qty + int(line.qty_done)
+        self.quantity = qty
+
     name = fields.Char()
     products = fields.Many2one('product.template')
-    quantity = fields.Char()
+    quantity = fields.Integer(compute='getItemQuantity')
     custody_quantity = fields.Char()
     amount_remaining = fields.Char()
     description = fields.Text()
