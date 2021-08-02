16d7483cfe7b3b66e78c33121b6773318ae6e619
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-02 12:19:27 +0300

    Update custody_employee.py

--- custom_hr_custody/models/custody_employee.py
+++ custom_hr_custody/models/custody_employee.py
@@ -9,13 +9,6 @@ class HrEmployeeCustody(models.Model):
     @api.model
     def getAllEmployeeCustody(self):
         employee_id = self.id
-        custody = self.env['hr.custody.lines'].sudo().search([('custody_id.employee','=',employee_id)])
+        custody = self.env['hr.custody.lines'].sudo().search([('custody_id.employee','=',employee_id),('custody_id.state','=','approved')])
         self.employee_custody = custody
-        # if self.parent_opportunity.id != False:
-        #     custody = self.env['hr.custody.lines'].sudo().search(['&','|',('default_access_emails','like','#'+str(self.env.uid)+'#'),'|',('stage_access_emails','like','#'+str(self.env.uid)+'#'),'|',('assigned_resources_access_emails','like','#'+str(self.env.uid)+'#'),('owner_ownerManager_emails','like','#'+str(self.env.uid)+'#'),('parent_opportunity','=',self.parent_opportunity.id),('id','!=',self.id)])
-        #     self.employee_custody = custody
-        # else:
-        #      self.employee_custody = []   
-    # related_project = fields.Many2many('project.project','related_project','name_seq',compute='getAllRelatedProject')
-
     employee_custody = fields.One2many('hr.custody.lines', 'custody_id',compute='getAllEmployeeCustody')
