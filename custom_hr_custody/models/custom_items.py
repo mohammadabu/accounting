9af891cc559f968e536c9cefccb6da06e982180c
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-02 15:31:59 +0300

    Update custom_items.py

--- custom_hr_custody/models/custom_items.py
+++ custom_hr_custody/models/custom_items.py
@@ -11,25 +11,6 @@ class HrCustomCustodyItems(models.Model):
     _description = 'Hr Custody Items'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-
-    @api.model
-    def getItemQuantity(self):
-        product_id = self.products.id
-        stock_move = self.env['stock.move.line'].sudo().search([('state','=','done'),('product_id','=',product_id)])
-        qty = 0
-        for line in stock_move:
-            qty = qty + int(line.qty_done)
-        self.quantity = qty
-
-    name = fields.Char()
-    products = fields.Many2one('product.template')
-    quantity = fields.Integer(readonly=True,default=0)
-    required_quantity = fields.Integer(required=True,default=1)
-    custody_quantity = fields.Char()
-    amount_remaining = fields.Char()
-    description = fields.Text()
-    
-
     @api.onchange('products')
     def onchange_products(self):        
         qty = 0
@@ -48,3 +29,12 @@ class HrCustomCustodyItems(models.Model):
             if qty < 0 :
                qty = 0 
         self.quantity = qty
+
+    name = fields.Char()
+    products = fields.Many2one('product.template')
+    quantity = fields.Integer(compute="onchange_products")
+    required_quantity = fields.Integer(required=True,default=1)
+    custody_quantity = fields.Char()
+    amount_remaining = fields.Char()
+    description = fields.Text()
+
