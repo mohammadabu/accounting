1d0f9d4ab8691f9823eaafc0000ce1672caa300b
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-02 15:24:32 +0300

    Update custom_items.py

--- custom_hr_custody/models/custom_items.py
+++ custom_hr_custody/models/custom_items.py
@@ -24,19 +24,27 @@ class HrCustomCustodyItems(models.Model):
     name = fields.Char()
     products = fields.Many2one('product.template')
     quantity = fields.Integer(readonly=True,default=0)
+    required_quantity = fields.Integer(required=True,default=1)
     custody_quantity = fields.Char()
     amount_remaining = fields.Char()
     description = fields.Text()
     
 
     @api.onchange('products')
-    def onchange_products(self):
-        # current_stage =  self._origin.project_stage.internal_id
-        # current_field = self._origin
+    def onchange_products(self):        
         qty = 0
         for rec in self:
+            # check product inventory qty 
             product_id = rec.products.id
             stock_move = self.env['stock.move.line'].sudo().search([('state','=','done'),('product_id','=',product_id)])
             for line in stock_move:
                 qty = qty + int(line.qty_done)
+            # check product custody qty
+            qty_used = 0
+            custody_items = self.env['hr.custody.items'].sudo().search([('products','=',product_id)])
+            for item in custody_items:
+                qty_used = qty_used + int(item.required_quantity)
+            qty = qty - qty_used
+            if qty < 0 :
+               qty = 0 
         self.quantity = qty
