7fd7c87e460d6dac591fcd7752661ea02fa290c8
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-03 12:54:15 +0300

    Update custom_items.py

--- custom_hr_custody/models/custom_items.py
+++ custom_hr_custody/models/custom_items.py
@@ -112,8 +112,9 @@ class HrCustomCustodyItems(models.Model):
     def unlink(self):
         rtn = super(HrCustomCustodyItems, self).unlink()
         current_id = self.id
-        custody_used_item = self.env['hr.custody.lines'].sudo().search([('custody_item','=',current_id)])
         _logger.info("custody_used_item")
+        _logger.info(current_id)
+        custody_used_item = self.env['hr.custody.lines'].sudo().search([('custody_item','=',current_id)])
         _logger.info(custody_used_item)
         if len(custody_used_item) > 0:
             _logger.info(len(custody_used_item))
