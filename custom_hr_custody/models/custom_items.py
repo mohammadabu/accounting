6b95c90639c2b2106d33a5a820b3e1aa890d0a2c
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-03 13:28:19 +0300

    Update custom_items.py

--- custom_hr_custody/models/custom_items.py
+++ custom_hr_custody/models/custom_items.py
@@ -110,9 +110,8 @@ class HrCustomCustodyItems(models.Model):
 
 
     def unlink(self):
-        rtn = super(HrCustomCustodyItems, self).unlink()
         current_id = self.id
-        _logger.info("custody_used_item") 
+        _logger.info("--------------------------custody_used_item---------------------------------------") 
         _logger.info(current_id)
         used_item = self.env['hr.custody.lines'].sudo().search([('custody_item','=',current_id)])
         used_item_new = self.env['hr.custody.lines'].sudo().search([('id','=',current_id)])
@@ -120,10 +119,7 @@ class HrCustomCustodyItems(models.Model):
         if len(used_item_new) > 0:
             _logger.info(len(used_item_new))
             raise exceptions.ValidationError(_("It cannot be deleted. You must delete all data related to it"))               
-        # _logger.info("sasadsdasdasadsdadssadsda")
-        # _logger.info(custody_used_item)
-        # _logger.info(current_id)
-        # _logger.info(self)
-        # _logger.info(rtn)
+        _logger.info("--------------------------custody_used_item---------------------------------------") 
         raise exceptions.ValidationError("dsadsa")
+        rtn = super(HrCustomCustodyItems, self).unlink()
         return rtn    
