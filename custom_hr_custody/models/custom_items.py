5ef03a0631f5bca5b64ab7c6a09e48bd6b2d408b
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-03 11:04:08 +0300

    Update custom_items.py

--- custom_hr_custody/models/custom_items.py
+++ custom_hr_custody/models/custom_items.py
@@ -66,7 +66,7 @@ class HrCustomCustodyItems(models.Model):
     name = fields.Char(required=True)
     products = fields.Many2one('product.template')
     quantity = fields.Integer(compute="onchange_products")
-    custody_quantity = fields.Integer(store=True,compute="onchange_products")
+    custody_quantity = fields.Integer(compute="onchange_products")
     custody_used = fields.Integer(store=True,compute="onchange_products")
     required_quantity = fields.Integer(required=True,default=1)
     amount_remaining = fields.Integer(store=True,compute="onchange_products")
