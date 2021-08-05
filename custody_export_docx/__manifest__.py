ac37315692d7e20c9947d4f458d2caf89ae2e9ac
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-05 09:58:53 +0300

    ed

--- custody_export_docx/__manifest__.py
+++ custody_export_docx/__manifest__.py
@@ -1,7 +1,7 @@
 {
     'name': 'Custodian Receipt Form',
     'version': '13.0.0.0',
-    'depends': ['hr'],
+    'depends': ['base','hr','custom_hr_custody'],
     'data': [
         'views/hr_custody.xml',
         'views/action_manager.xml',
