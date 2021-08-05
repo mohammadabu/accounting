ae5ad14d177498f1e771b530c39fb12e54490f2a
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-05 10:45:00 +0300

    Update custodian_receipt_export_docx.py

--- custody_export_docx/reports/custodian_receipt_export_docx.py
+++ custody_export_docx/reports/custodian_receipt_export_docx.py
@@ -49,6 +49,7 @@ class CustodianReceiptExportDocx(models.AbstractModel):
         hdr_cells[2].text = 'Price'
         for ID, name, price in record:
             row_Cells = menuTable.add_row().cells
+            self.pool.get("report.custody_export_docx.custodian_receipt_docx").set_cell_border(self,row_Cells[0])
             row_Cells[0].text = str(ID)
             row_Cells[1].text = name
             row_Cells[2].text = str(price)
@@ -152,3 +153,43 @@ class CustodianReceiptExportDocx(models.AbstractModel):
         num = num.replace("9", "٩", 15)
         num = num.replace("0", "٠", 15)
         return num
+
+
+    def set_cell_border(self ,cell: _Cell, **kwargs):
+        """
+        Set cell`s border
+        Usage:
+
+        set_cell_border(
+            cell,
+            top={"sz": 12, "val": "single", "color": "#FF0000", "space": "0"},
+            bottom={"sz": 12, "color": "#00FF00", "val": "single"},
+            start={"sz": 24, "val": "dashed", "shadow": "true"},
+            end={"sz": 12, "val": "dashed"},
+        )
+        """
+        tc = cell._tc
+        tcPr = tc.get_or_add_tcPr()
+
+        # check for tag existnace, if none found, then create one
+        tcBorders = tcPr.first_child_found_in("w:tcBorders")
+        if tcBorders is None:
+            tcBorders = OxmlElement('w:tcBorders')
+            tcPr.append(tcBorders)
+
+        # list over all available tags
+        for edge in ('start', 'top', 'end', 'bottom', 'insideH', 'insideV'):
+            edge_data = kwargs.get(edge)
+            if edge_data:
+                tag = 'w:{}'.format(edge)
+
+                # check for tag existnace, if none found, then create one
+                element = tcBorders.find(qn(tag))
+                if element is None:
+                    element = OxmlElement(tag)
+                    tcBorders.append(element)
+
+                # looks like order of attributes is important
+                for key in ["sz", "val", "color", "space", "shadow"]:
+                    if key in edge_data:
+                        element.set(qn('w:{}'.format(key)), str(edge_data[key]))
