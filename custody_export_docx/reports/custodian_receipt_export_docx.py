71625ba75416ba775d9e9f68953e05c3e4a00387
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-05 11:09:57 +0300

    Update custodian_receipt_export_docx.py

--- custody_export_docx/reports/custodian_receipt_export_docx.py
+++ custody_export_docx/reports/custodian_receipt_export_docx.py
@@ -155,28 +155,28 @@ class CustodianReceiptExportDocx(models.AbstractModel):
         num = num.replace("0", "٠", 15)
         return num
 
-def modifyBorder(self,table):
-    tbl = table._tbl # get xml element in table
-    for cell in tbl.iter_tcs():
-        tcPr = cell.tcPr # get tcPr element, in which we can define style of borders
-        tcBorders = OxmlElement('w:tcBorders')
-        top = OxmlElement('w:top')
-        top.set(qn('w:val'), 'nil')
-        
-        left = OxmlElement('w:left')
-        left.set(qn('w:val'), 'nil')
-        
-        bottom = OxmlElement('w:bottom')
-        bottom.set(qn('w:val'), 'nil')
-        bottom.set(qn('w:sz'), '4')
-        bottom.set(qn('w:space'), '0')
-        bottom.set(qn('w:color'), 'auto')
+    def modifyBorder(self,table):
+        tbl = table._tbl # get xml element in table
+        for cell in tbl.iter_tcs():
+            tcPr = cell.tcPr # get tcPr element, in which we can define style of borders
+            tcBorders = OxmlElement('w:tcBorders')
+            top = OxmlElement('w:top')
+            top.set(qn('w:val'), 'nil')
+            
+            left = OxmlElement('w:left')
+            left.set(qn('w:val'), 'nil')
+            
+            bottom = OxmlElement('w:bottom')
+            bottom.set(qn('w:val'), 'nil')
+            bottom.set(qn('w:sz'), '4')
+            bottom.set(qn('w:space'), '0')
+            bottom.set(qn('w:color'), 'auto')
 
-        right = OxmlElement('w:right')
-        right.set(qn('w:val'), 'nil')
+            right = OxmlElement('w:right')
+            right.set(qn('w:val'), 'nil')
 
-        tcBorders.append(top)
-        tcBorders.append(left)
-        tcBorders.append(bottom)
-        tcBorders.append(right)
-        tcPr.append(tcBorders)
+            tcBorders.append(top)
+            tcBorders.append(left)
+            tcBorders.append(bottom)
+            tcBorders.append(right)
+            tcPr.append(tcBorders)
