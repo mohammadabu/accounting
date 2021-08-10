30f099794bd379c2f0a629093821908a18cfbe49
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-10 13:44:07 +0300

    Update custodian_receipt_export_docx.py

--- custody_export_docx/reports/custodian_receipt_export_docx.py
+++ custody_export_docx/reports/custodian_receipt_export_docx.py
@@ -653,7 +653,7 @@ class CustodianReceiptExportDocx(models.AbstractModel):
         last_runner_10 = last_p3.add_run('( ')
         last_runner_10.style = font_headerTable_3
 
-        last_runner_8 = last_p3.add_run(': لا، للأسباب التالية  ')
+        last_runner_8 = last_p3.add_run(' لا، للأسباب التالية  ')
         last_runner_8.style = font_headerTable_3
         
 
