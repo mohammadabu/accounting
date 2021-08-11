27b139877d7d0f06c98b11f6dfba56bf628589ba
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-11 09:39:30 +0300

    ed

--- job_assignment/models/custom_job_assignment.py
+++ job_assignment/models/custom_job_assignment.py
@@ -13,13 +13,9 @@ class HrCustomJobAssignment(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
     name = fields.Char(required=True, readonly=True)
-    # employee = fields.Many2one('hr.employee',required=True, readonly=True ,states={'draft': [('readonly', False)]},default=_getDefaultEmployee)
-    # reason = fields.Char(required="1")
-    # notes = fields.Text()
-    # state = fields.Selection([('draft', 'Draft'), ('to_approve', 'Waiting For Approval'), ('approved', 'Approved'),
-    #                         ('rejected', 'Refused')], string='Status', default='draft',
-    #                         track_visibility='always')
-    # custody_lines = fields.One2many('hr.custody.lines', 'custody_id',ondelete='cascade')      
-    # rejected_reason = fields.Text(string='Rejected Reason', copy=False, readonly=1, help="Reason for the rejection")                   
+    employee = fields.Many2one('hr.employee',required=True, readonly=True ,states={'draft': [('readonly', False)]},default=_getDefaultEmployee)
+    state = fields.Selection([('draft', 'Draft'), ('to_approve', 'Waiting For Approval'), ('approved', 'Approved'),
+                            ('rejected', 'Refused')], string='Status', default='draft',
+                            track_visibility='always')
 
     
