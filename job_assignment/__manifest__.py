34296422b89ec72c0c743083d62baa274bdd54b9
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-11 11:47:25 +0300

    Update __manifest__.py

--- job_assignment/__manifest__.py
+++ job_assignment/__manifest__.py
@@ -4,7 +4,6 @@
     'depends': ['hr', 'mail','hr_holidays'],
     'data': [
         'views/custom_job_assignment_view.xml',
-        'data/data.xml',
         # 'views/custody_employee_view.xml',
         # 'views/custom_custody_view.xml',
         # 'views/custom_items_view.xml',
