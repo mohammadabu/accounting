3525ac9f85a82856ac02cc393696421d6fde76be
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-11 11:14:46 +0300

    ed

--- job_assignment/__manifest__.py
+++ job_assignment/__manifest__.py
@@ -1,9 +1,10 @@
 {
     'name': 'Custom Etmam Job Assignment',
     'version': '13.0.1.0.0',
-    'depends': ['hr', 'mail'],
+    'depends': ['hr', 'mail','hr_holidays'],
     'data': [
         'views/custom_job_assignment_view.xml',
+        'data/data.xml',
         # 'views/custody_employee_view.xml',
         # 'views/custom_custody_view.xml',
         # 'views/custom_items_view.xml',
