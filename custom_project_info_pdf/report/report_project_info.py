9bcc930f0b8e9d9a0f8a5d191b3c26a71aebdf5c
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-14 17:47:59 +0300

    ed

--- custom_project_info_pdf/report/report_project_info.py
+++ custom_project_info_pdf/report/report_project_info.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 class ReportProjectInfo(models.AbstractModel):
     _name = 'report.custom_project_info_pdf.report_project_pdf'
 
-    def get_timesheets(self, docs):
+    def get_project_info(self, docs):
 
         # if docs.from_date and docs.to_date:
         #     rec = self.env['account.analytic.line'].search([('user_id', '=', docs.employee[0].id),
@@ -20,58 +20,60 @@ class ReportProjectInfo(models.AbstractModel):
         # else:
         #     rec = self.env['account.analytic.line'].search([('user_id', '=', docs.employee[0].id)])
 
-        rec = self.env['project.project'].search([('project.project', '=', docs.employee[0].id)])
+        rec = self.env['project.project'].search([('active', '=', 1)])
 
 
         records = []
-        total = 0
         for r in rec:
-            vals = {'project': r.project_id.name,
-                    'user': r.user_id.partner_id.name,
-                    'duration': r.unit_amount,
-                    'date': r.date,
+            vals = {'name_seq': r.name_seq,
+                    'name': r.name,
+                    'stage': r.project_stage.name,
+                    'parent_opportunity	': r.parent_opportunity.name,
                     }
-            total += r.unit_amount
             records.append(vals)
-        return [records, total]
+        return [records]
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        """we are overwriting this function because we need to show values from other models in the report
-        we pass the objects in the docargs dictionary"""
-        docs = self.env['timesheet.wizard'].browse(self.env.context.get('active_id'))
-        identification = []
-        for i in self.env['hr.employee'].search([('user_id', '=', docs.employee[0].id)]):
-            if i:
-                identification.append({'id': i.id, 'name': i.name})
-        timesheets = self.get_timesheets(docs)
-        company_name = self.env['res.company'].search([('name', '=', docs.employee[0].company_id.name)])
-        period = None
-        if docs.from_date and docs.to_date:
-            period = "From " + str(docs.from_date) + " To " + str(docs.to_date)
-        elif docs.from_date:
-            period = "From " + str(docs.from_date)
-        elif docs.from_date:
-            period = " To " + str(docs.to_date)
-        if len(identification) > 1:
-            return {
-                'doc_ids': self.ids,
-                # 'doc_model': self.model,
-                'docs': docs,
-                'timesheets': timesheets[0],
-                'total': timesheets[1],
-                'company': company_name,
-                'identification': identification,
-                'period': period,
-            }
-        else:
-            return {
-                'doc_ids': self.ids,
-                # 'doc_model': self.model,
-                'docs': docs,
-                'timesheets': timesheets[0],
-                'total': timesheets[1],
-                'identification': identification,
-                'company': company_name,
-                'period': period,
-            }
+        docs = self.env['project_info.wizard'].browse(self.env.context.get('active_id'))
+        # identification = []
+        # for i in self.env['hr.employee'].search([('user_id', '=', docs.employee[0].id)]):
+        #     if i:
+        #         identification.append({'id': i.id, 'name': i.name})
+        project_info = self.get_project_info(docs)
+        # company_name = self.env['res.company'].search([('name', '=', docs.employee[0].company_id.name)])
+        # period = None
+        # if docs.from_date and docs.to_date:
+        #     period = "From " + str(docs.from_date) + " To " + str(docs.to_date)
+        # elif docs.from_date:
+        #     period = "From " + str(docs.from_date)
+        # elif docs.from_date:
+        #     period = " To " + str(docs.to_date)
+        # if len(identification) > 1:
+        #     return {
+        #         'doc_ids': self.ids,
+        #         # 'doc_model': self.model,
+        #         'docs': docs,
+        #         'timesheets': timesheets[0],
+        #         'total': timesheets[1],
+        #         'company': company_name,
+        #         'identification': identification,
+        #         'period': period,
+        #     }
+        # else:
+        #     return {
+        #         'doc_ids': self.ids,
+        #         # 'doc_model': self.model,
+        #         'docs': docs,
+        #         'timesheets': timesheets[0],
+        #         'total': timesheets[1],
+        #         'identification': identification,
+        #         'company': company_name,
+        #         'period': period,
+        #     }
+
+        return {
+            'doc_ids': self.ids,
+            'docs': docs,
+            'project_info': timesheets[0],
+        }
