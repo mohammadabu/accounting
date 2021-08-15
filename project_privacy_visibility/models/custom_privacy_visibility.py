981dbbcf6f7d4a545361396e4e39a194a5596771
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-15 14:50:16 +0300

    Update custom_privacy_visibility.py

--- project_privacy_visibility/models/custom_privacy_visibility.py
+++ project_privacy_visibility/models/custom_privacy_visibility.py
@@ -32,15 +32,40 @@ class CustomPrivacyVisibility(models.Model):
             self.hide_product_id = 'no'
         else:
             self.hide_product_id = 'yes'    
-        # if self.employee_id.parent_id != False:
-        #     if self.employee_id.parent_id.user_id.id == self.env.user.id:
-        #         self.hide_product_id = 'no'
-        #     else:
-        #         self.hide_product_id = 'yes'
-        # else:
-        #     self.hide_product_id = 'yes' 
-    
 
+
+    def checkUserEmail(self):
+        all_project = self.env['project.project'].sudo().search([])
+        for project in all_project:
+            all_user_emails = ''
+            if project.privacy_visibility == "department":
+                project_department = project.department.id
+                # get all employee department
+                employees = self.env['hr.employee'].sudo().search([('department_id','=',project_department)])
+                for emp in employees:
+                    if emp.user_id != False:
+                        user_email = self.env['res.users'].search([('id','=',emp.user_id.id)])
+                        if user_email.login != False:
+                            if all_user_emails != False:
+                                if user_email.login not in all_user_emails:
+                                    all_user_emails = all_user_emails + "," + user_email.login
+                            else:
+                                all_user_emails = user_email.login
+            project.user_emails = all_user_emails
+                
+
+
+
+
+    def write(self,values):
+        befory_edit_privacy_visibility = self.privacy_visibility
+        before_edit_department = self.department.id
+        rtn = super(CustomPrivacyVisibility,self).write(values)
+        after_edit_privacy_visibility = self.privacy_visibility
+        after_edit_department = self.department.id
+        if befory_edit_privacy_visibility != after_edit_privacy_visibility or before_edit_department != after_edit_department:
+            self.pool.get("project.project").checkUserEmail(self)
+        return rtn     
 
 
  
