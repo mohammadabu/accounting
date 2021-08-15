cc4457bf6b086d3a8a7a6a06e2164a16f4ecb9d6
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-15 11:11:13 +0300

    Update custom_privacy_visibility.py

--- project_privacy_visibility/models/custom_privacy_visibility.py
+++ project_privacy_visibility/models/custom_privacy_visibility.py
@@ -13,7 +13,30 @@ class CustomPrivacyVisibility(models.Model):
     privacy_visibility = fields.Selection(selection_add=[('department', 'Department')])
 
     department = fields.Many2one('hr.department')
- 
+
+    hide_product_id = fields.Selection(
+        [
+            ('yes', 'yes'),
+            ('no','no')
+        ]
+        ,compute='_compute_hide_product_id'
+    )
+    @api.depends('department')
+    def _compute_hide_product_id(self):
+        employee = self.env['hr.employee'].sudo().search([('user_id','=',self.env.user.id),('department_id','=',self.department.id)])
+        if len(employee) > 0 :
+            self.hide_product_id = 'no'
+        else:
+            self.hide_product_id = 'yes'    
+        # if self.employee_id.parent_id != False:
+        #     if self.employee_id.parent_id.user_id.id == self.env.user.id:
+        #         self.hide_product_id = 'no'
+        #     else:
+        #         self.hide_product_id = 'yes'
+        # else:
+        #     self.hide_product_id = 'yes' 
+    
+
 
 
  
