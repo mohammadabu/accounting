b326277ac1bb08b9eac99beeafdbc2d990d7d675
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-15 22:10:15 +0300

    Update custom_privacy_visibility.py

--- project_privacy_visibility/models/custom_privacy_visibility.py
+++ project_privacy_visibility/models/custom_privacy_visibility.py
@@ -79,6 +79,58 @@ class CustomPrivacyVisibility(models.Model):
             self.pool.get("project.project").checkUserEmail(self)
         return rtn     
 
+
+
+    def checkUserEmailCreate(self):            
+        all_user_emails = False
+        if self.privacy_visibility == "department":
+            project_department = self.department.id
+            # get all employee department
+            employees = self.env['hr.employee'].sudo().search([('department_id','=',project_department)])
+            for emp in employees:
+                if emp.user_id != False:
+                    user_email = self.env['res.users'].search([('id','=',emp.user_id.id)])
+                    if user_email.login != False:
+                        if all_user_emails != False:
+                            if user_email.login not in all_user_emails:
+                                all_user_emails = all_user_emails + "," + user_email.login
+                        else:
+                            all_user_emails = user_email.login
+
+            if self.department.manager_id != False:
+                manager_department = self.department.manager_id
+                if manager_department.user_id != False:
+                    # get manager of department
+                    user_email_dep = self.env['res.users'].search([('id','=',manager_department.user_id.id)])
+                    if user_email_dep.login != False:
+                        if all_user_emails != False:
+                            if user_email_dep.login not in all_user_emails:
+                                all_user_emails = all_user_emails + "," + user_email_dep.login
+                        else:
+                            all_user_emails = user_email_dep.login  
+
+                    # get all parent manager of department
+                    i = 0
+                    count_final = 0
+                    while i <= 10:
+                        if count_final < 200:
+                            _logger.info("start")
+                            _logger.info(manager_department.parent_id)
+                            _logger.info(manager_department.parent_id.name)
+                            if len(manager_department.parent_id) > 0:
+                                if manager_department.parent_id.user_id != False:
+                                    if all_user_emails != False:
+                                        if manager_department.parent_id.user_id.login not in all_user_emails:
+                                            all_user_emails = all_user_emails + "," + manager_department.parent_id.user_id.login
+                                    else:
+                                        all_user_emails = manager_department.parent_id.user_id.login 
+                                manager_department = manager_department.parent_id 
+                                _logger.info(manager_department)   
+                            count_final = count_final + 1                    
+                        else:
+                            break
+        self.user_emails = all_user_emails
+
     @api.model
     def create(self,vals):
         try:   
@@ -88,8 +140,9 @@ class CustomPrivacyVisibility(models.Model):
                 privacy_visibility = vals['privacy_visibility']
             if "department" in vals:
                 department = vals['department']
-
+            _logger.info("123456789")
             if privacy_visibility == "department":
+                _logger.info("123452222226789")
                 self.pool.get("project.project").checkUserEmail(rtn)
 
         except:
