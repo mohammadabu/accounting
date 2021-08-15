702f5c7a059b590dfd51d1e75b297b2e8700b583
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-15 13:28:03 +0300

    Update __manifest__.py

--- project_privacy_visibility/__manifest__.py
+++ project_privacy_visibility/__manifest__.py
@@ -3,8 +3,8 @@
     'version': '13.0.1.0.0',
     'depends': ['project','hr'],
     'data': [
-        'security/privacy_visibility_security.xml',
         'views/custom_privacy_visibility_view.xml',
+        'security/privacy_visibility_security.xml',
         # 'security/ir.model.access.csv',
     ],
 }
