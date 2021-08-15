b683379bc8f4e2306866999ad14f712ea0fc440c
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-15 13:33:17 +0300

    Update __manifest__.py

--- project_privacy_visibility/__manifest__.py
+++ project_privacy_visibility/__manifest__.py
@@ -1,11 +1,10 @@
 {
     'name': 'Custom Etmam Project Privacy Visibility',
     'version': '13.0.1.0.0',
-    'depends': ['project','hr'],
+    'depends': ['hr', 'project'],
     'data': [
-        # 'views/custom_privacy_visibility_view.xml',
-        # 'security/privacy_visibility_security.xml',
+        'views/custom_privacy_visibility_view.xml',
+        'security/privacy_visibility_security.xml',
         # 'security/ir.model.access.csv',
     ],
 }
- 
