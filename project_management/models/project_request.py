d494fa14a2cd699c3d8e4c9ae3fe19dded0f8bbc
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-23 13:31:43 +0300

    ed

--- project_management/models/project_request.py
+++ project_management/models/project_request.py
@@ -1,5 +1,11 @@
 
-from odoo import models, fields, api, _
+
+from datetime import date, datetime, timedelta
+from odoo import models, fields, api,exceptions
+from odoo.tools.translate import _
+import logging
+_logger = logging.getLogger(__name__)
+
 
 
 class ProjectRequest(models.Model):
@@ -7,3 +13,16 @@ class ProjectRequest(models.Model):
     _name = 'project.request'
     _description = 'Project Request'
     _inherit = ['mail.thread', 'mail.activity.mixin']
+
+    project_name        = fields.Char(required=True)
+    owner_name          = fields.Many2one('hr.employee',string='Owner Project Name',required=True)
+    owner_dept          = fields.Many2one('hr.employee',string='Owner Dept.',required=True)
+    expected_start      = fields.Date(string="Expected start date",required=True)
+    expected_end        = fields.Date(string="Expected end date",required=True)
+    estimated_budget    = fields.Float(string="Estimated budget",required=True)
+    project_type        = fields.Selection([('internal', 'Internal'),('external', 'External')],required=True)
+    project_description = fields.Html()  
+    request_date        = fields.Date(compute='_compute_request_date')
+    @api.depends()
+    def _compute_request_date(self):
+        self.request_date = datetime.today()
