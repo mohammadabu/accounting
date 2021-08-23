41cfda862d017f182017a7beedf750f0f2d2c1bf
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-23 15:14:42 +0300

    ed

--- project_management/models/project_request.py
+++ project_management/models/project_request.py
@@ -14,29 +14,44 @@ class ProjectRequest(models.Model):
     _description = 'Project Request'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
+    def _getDefaultEmployee(self):
+        user_id = self.env.user.id
+        employee = self.env['hr.employee'].sudo().search([('user_id','=',user_id)],limit=1)
+        if len(employee) > 0 :
+            return employee.id
+        else:
+            return False 
+
     name                = fields.Char(string="Project Name",required=True,track_visibility=1)
-    owner_name          = fields.Many2one('hr.employee',string='Owner Project Name',required=True,track_visibility=1)
-    owner_dept          = fields.Many2one('hr.employee',string='Owner Dept.',required=True,track_visibility=1)
+    owner_name          = fields.Many2one('hr.employee',string='Owner Project Name',required=True,default=_getDefaultEmployee,track_visibility=1)
+    owner_dept          = fields.Many2one('hr.department',string='Owner Dept.',required=True,track_visibility=1)
     expected_start      = fields.Date(string="Expected start date",required=True,track_visibility=1)
     expected_end        = fields.Date(string="Expected end date",required=True,track_visibility=1)
     estimated_budget    = fields.Float(string="Estimated budget",required=True,track_visibility=1)
     project_type        = fields.Selection([('internal', 'Internal'),('external', 'External')],required=True,track_visibility=1)
     project_description = fields.Html()  
-    # request_date        = fields.Date(compute='_compute_request_date')
     request_date        = fields.Date(readonly=True)
-    # @api.depends()
-    # def _compute_request_date(self):
-    #     self.request_date = datetime.today()
+    justifications      = fields.One2many('project.request.justification','request_id',ondelete='cascade') 
+    objectives          = fields.One2many('project.main.objectives','request_id',ondelete='cascade') 
+    deliverables        = fields.One2many('project.main.deliverables','request_id',ondelete='cascade') 
 
-    justifications = fields.One2many('project.request.justification','request_id',ondelete='cascade') 
-    objectives = fields.One2many('project.main.objectives','request_id',ondelete='cascade') 
-    deliverables = fields.One2many('project.main.deliverables','request_id',ondelete='cascade') 
+
+    user_department = fields.Integer(compute='_compute_user_department')
+    current_user = fields.Many2one('res.users', default=lambda self : self.env.uid)
+    @api.depends('current_user')
+    def _compute_user_department(self):
+        user_employee = self.env['hr.employee'].sudo().search([('user_id','=',self.current_user.id)],limit=1) 
+        if len(user_employee) > 0:
+            self.user_department = int(user_employee.department_id.id)
+        else:
+            self.user_department = 0 
+
 
     @api.model
     def create(self,vals):
         vals['request_date'] = datetime.today()
         rtn = super(ProjectRequest,self).create(vals)
-        return rtn
+        return rtn 
 
 
     def unlink(self):
