c91dbf2da65776bebe9bcc2ace063b78bb57586c
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-23 14:28:57 +0300

    ed

--- project_management/models/project_request.py
+++ project_management/models/project_request.py
@@ -14,15 +14,24 @@ class ProjectRequest(models.Model):
     _description = 'Project Request'
     _inherit = ['mail.thread', 'mail.activity.mixin']
 
-    name                = fields.Char(string="Project Name",required=True)
-    owner_name          = fields.Many2one('hr.employee',string='Owner Project Name',required=True)
-    owner_dept          = fields.Many2one('hr.employee',string='Owner Dept.',required=True)
-    expected_start      = fields.Date(string="Expected start date",required=True)
-    expected_end        = fields.Date(string="Expected end date",required=True)
-    estimated_budget    = fields.Float(string="Estimated budget",required=True)
-    project_type        = fields.Selection([('internal', 'Internal'),('external', 'External')],required=True)
+    name                = fields.Char(string="Project Name",required=True,track_visibility=1)
+    owner_name          = fields.Many2one('hr.employee',string='Owner Project Name',required=True,track_visibility=1)
+    owner_dept          = fields.Many2one('hr.employee',string='Owner Dept.',required=True,track_visibility=1)
+    expected_start      = fields.Date(string="Expected start date",required=True,track_visibility=1)
+    expected_end        = fields.Date(string="Expected end date",required=True,track_visibility=1)
+    estimated_budget    = fields.Float(string="Estimated budget",required=True,track_visibility=1)
+    project_type        = fields.Selection([('internal', 'Internal'),('external', 'External')],required=True,track_visibility=1)
     project_description = fields.Html()  
     request_date        = fields.Date(compute='_compute_request_date')
     @api.depends()
     def _compute_request_date(self):
         self.request_date = datetime.today()
+
+
+    justifications = fields.One2many('project.request.justification', 'Project Request Justification',ondelete='cascade') 
+
+class ProjectRequestJustification(models.Model):
+
+    _name = 'project.request.justification' 
+
+    name  = fields.Char(string="Justifications")
