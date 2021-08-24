4f96e2dae679e83244b00b2ad49c4cf2d116f73d
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-24 10:15:46 +0300

    Update project_request.py

--- project_management/models/project_request.py
+++ project_management/models/project_request.py
@@ -35,8 +35,13 @@ class ProjectRequest(models.Model):
     objectives          = fields.One2many('project.main.objectives','request_id',ondelete='cascade') 
     deliverables        = fields.One2many('project.main.deliverables','request_id',ondelete='cascade') 
     user_department     = fields.Integer(compute='_compute_user_department')
-    current_user        = fields.Many2one('res.users', default=lambda self : self.env.uid)
+    current_user        = fields.Many2one('res.users',compute='_compute_user_login')
     
+
+    @api.depends()
+    def _compute_user_login(self):
+        self.current_user = self.env.uid
+
     @api.depends('current_user')
     def _compute_user_department(self):
         user_employee = self.env['hr.employee'].sudo().search([('user_id','=',self.current_user.id)],limit=1) 
