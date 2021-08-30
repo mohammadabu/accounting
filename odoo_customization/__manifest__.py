4bb920f2f7a59670a083c1e0c7ecde0ac5d2c0ab
mohammadabu <55664960+mohammadabu@users.noreply.github.com>
2021-08-30 10:33:54 +0300

    ed

--- odoo_customization/__manifest__.py
+++ odoo_customization/__manifest__.py
@@ -1,8 +1,9 @@
 {
     'name': 'Odoo Customization',
     'version': '13.0.1.0.0',
-    'depends': ['hr'],
+    'depends': ['hr','hr_attendance'],
     'data': [
         'views/hr_employee_view.xml',
+        'security/ir.model.access.csv'
     ],
 }
